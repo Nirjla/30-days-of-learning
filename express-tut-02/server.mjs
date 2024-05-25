ac278d8903dc1f7fe2e00998460ea15c0c7371c8
Nirjla <shakyanirjala6@gmail.com>
2024-05-25 18:29:21 +0545

    CRUD completed

--- express-tut-02/server.mjs
+++ express-tut-02/server.mjs
@@ -14,7 +14,21 @@ app.get("/", (req, res) => {
 app.get("/api/products", async (req, res) => {
   try {
     const product = await Product.find({});
-    res.status(500).send(product)
+    res.status(500).send(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    // const { id } = req;
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).send("Product Not Found");
+    }
+    res.status(200).send(product);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -24,6 +38,35 @@ app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(201).send(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      res.status(404).send("Product Not Found");
+    }
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).send("Product Not Found");
+    }
+    res.status(200).send(product);
+
   } catch (err) {
     res.status(500).send(err.message);
   }
